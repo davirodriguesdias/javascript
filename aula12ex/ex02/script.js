9a669347d5155a4741c77670f6f3f0189cd27f71
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2025-05-15 23:06:09 -0300

    calculadora de idade

--- aula12ex/ex02/script.js
+++ aula12ex/ex02/script.js
@@ -6,6 +6,9 @@ var sys = new Date()
 
 var txtsex = document.getElementsByName('radiosex')
 
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+
 function calcular() {
 
     var ano = Number(txtano.value)
@@ -18,30 +21,46 @@ function calcular() {
     if(txtsex[0].checked) {
         genero = 'um Homem'
         if(idade < 0) {
-            //espermatozoide
+            img.setAttribute('src', 'imagens/esper.jpeg')
         } else if(idade <= 2) {
-            //bebê
+            img.setAttribute('src', 'imagens/bebeboy.jpeg')
         } else if(idade <= 10) {
-            //criança
-        } else if(idade <= 12) {
-            //pre-adolescente
-        } else if(idade <= 17) {
-            //adolescente
+            img.setAttribute('src', 'imagens/kidboy.webp')
+        } else if(idade <= 15) {
+            img.setAttribute('src', 'imagens/teenboy.webp')
         } else if(idade <= 29) {
-            //jovem adulto
+            img.setAttribute('src', 'imagens/teenadultboy.webp')
         } else if(idade <= 59) {
-            //adulto
+            img.setAttribute('src', 'imagens/adultboy.jpeg')
         } else if(idade <= 90) {
-            //idoso
+            img.setAttribute('src', 'imagens/oldman.jpeg')
         } else{
-            //morte
+           img.setAttribute('src', 'imagens/death.jpg')
         }
     } else if(txtsex[1].checked) {
         genero = 'uma Mulher'
+        if(idade < 0) {
+            img.setAttribute('src', 'imagens/esper.jpeg')
+        } else if(idade <= 2) {
+            img.setAttribute('src', 'imagens/bebe.jpeg')
+        } else if(idade <= 10) {
+            img.setAttribute('src', 'imagens/kidgirl.webp')
+        } else if(idade <= 15) {
+            img.setAttribute('src', 'imagens/teengirl.webp')
+        } else if(idade <= 29) {
+            img.setAttribute('src', 'imagens/teenadultgirl.webp')
+        } else if(idade <= 59) {
+            img.setAttribute('src', 'imagens/adultgirl.jpeg')
+        } else if(idade <= 90) {
+            img.setAttribute('src', 'imagens/oldwoman.jpeg')
+        } else{
+           img.setAttribute('src', 'imagens/death.jpg')
+        }
     }
 
     document.querySelector('section#content').style.height = '420px'
     res.innerHTML = `Você é ${genero} de ${idade} anos`
+    document.querySelector('div#result').appendChild(img)
 
     
 }
