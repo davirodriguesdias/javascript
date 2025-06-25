20cc2ad6f1de426aa161c070bce063c260c49f4f
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2025-06-24 23:36:37 -0300

    Estudando vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,27 @@
+let num = [5, 8, 2, 9, 3]
+
+// num.sort()
+
+// num.push(1)
+
+// console.log(num)
+
+// console.log(`O vetor tem ${num.length} posições`)
+// console.log(num[3])
+
+
+// for(let pos = 0; pos < num.length; pos++) {
+//     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+// }
+
+// for(let pos in num) {
+//     console.log(num[pos])
+// }
+
+// let pos = num.indexOf(7)
+
+// if(pos == -1) {
+//     console.log('O valor não foi encontrado')
+// } else {
+//     console.log(`O valor está na posição ${pos}`)
+// }
